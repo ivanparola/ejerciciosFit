6063ade563b2830a698a839d905d4d76045ffe23
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-10-20 21:58:14 -0300

    eliminamos json de prueba

--- routes/users.js
+++ routes/users.js
@@ -3,64 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource'); 
-  res.json([
-    {
-      _id: 1,
-      nombre: 'Nicolas',
-      apellido: 'Bellin'
-    },
-    {
-      _id: 2,
-      nombre: 'Andrea',
-      apellido: 'Felizzola'
-    },
-    {
-      _id: 3,
-      nombre: 'Ignacio',
-      apellido: 'Giss'
-    },
-    {
-      _id: 4,
-      nombre: 'Ivan',
-      apellido: 'Parola'
-    },
-    {
-      _id: 5,
-      nombre: 'Jeronimo',
-      apellido: 'Sanchez'
-    }
-  ])
+   res.send('respond with a resource'); 
 });
 
 router.get('/:id', function(req,res,next){
-  const users = [
-    {
-      _id: 1,
-      nombre: 'Nicolas',
-      apellido: 'Bellin'
-    },
-    {
-      _id: 2,
-      nombre: 'Andrea',
-      apellido: 'Felizzola'
-    },
-    {
-      _id: 3,
-      nombre: 'Ignacio',
-      apellido: 'Giss'
-    },
-    {
-      _id: 4,
-      nombre: 'Ivan',
-      apellido: 'Parola'
-    },
-    {
-      _id: 5,
-      nombre: 'Jeronimo',
-      apellido: 'Sanchez'
-    }
-  ];
 
   res.json(users.filter(user => user._id == parseInt(req.params.id)));
 });
