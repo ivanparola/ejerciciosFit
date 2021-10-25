2c2a0e74adb511d02798fc027a85e35fcf172a14
Nacho Giss <giss.ignacio@gmail.com>
2021-10-24 21:52:24 -0300

    User Authorization

--- routes/ejercicios.js
+++ routes/ejercicios.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data/ejercicio');
+const auth = require('../middleware/auth')
 
-router.get('/', async (req, res) =>{
+router.get('/', auth, async (req, res) =>{
     let ejercicios = await data.getEjercicios();
 
     res.json(ejercicios);
