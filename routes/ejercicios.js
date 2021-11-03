d00547fa412fec1e961433ec102699e450ef9ad8
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-11-03 14:32:21 -0300

    otra forma: get ejercicios y get ejercicios por id

--- routes/ejercicios.js
+++ routes/ejercicios.js
@@ -3,10 +3,22 @@ const router = express.Router();
 const data = require('../data/ejercicio');
 const auth = require('../middleware/auth')
 
+/* otra forma, esta ok? */
+router.get('api/ejercicios/', async (req, res) =>{
+  let ejercicios = await data.getEjercicios();
+  res.json(ejercicios);
+});
+
+router.get('api/ejercicios/:id', async (req, res) =>{
+  const id = req.params.id;
+  res.json(ejercicios.filter(data => data.id == parseInt(id))); 
+  res.end();
+});
+/* otra forma, esta ok? */
 
 router.get('/', auth, async (req, res) =>{
-    let ejercicios = await data.getEjercicios(req.query.tipo, req.query.dificultad);
-    res.json(ejercicios);
+  let ejercicios = await data.getEjercicios(req.query.tipo, req.query.dificultad);
+  res.json(ejercicios);
 });
 
 router.get('/:id', auth, async (req, res) =>{
