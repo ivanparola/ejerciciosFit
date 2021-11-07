b65d102eb1795cbb22086d62a92612ea69894c42
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-11-06 21:07:53 -0300

    getUser & setFavorito

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource'); 
 });
 
-router.get('/:id', function(req,res,next){
-  res.json(users.filter(user => user._id == parseInt(req.params.id)));
+router.post('/login', async(req, res)=>{
+  try {
+    const user = await data.findByCredentials(req.body.email, req.body.password);
+    const token = await data.generatedAuthToken(user);
+    res.send({user, token});
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
+
+router.get('/:id', async(req,res,next)=>{
+  res.json(await data.getUser(req.params.id));
 });
 
 router.post('/', async (req, res)=>{
@@ -17,16 +27,10 @@ router.post('/', async (req, res)=>{
   res.send(result);
 });
 
-router.post('/login', async(req, res)=>{
-  try {
-    const user = await data.findByCredentials(req.body.email, req.body.password);
-    const token = await data.generatedAuthToken(user);
-
-    res.send({user, token});
-
-  } catch (error) {
-    res.status(401).send(error.message);
-  }
-});
+router.put('/favoritos/:id', async(req, res)=>{
+  let favoritos = await data.setFavorito(req.params.id, "6186e060147fab3176ceb300");
+  res.json(favoritos);
+  res.end();
+})
 
 module.exports = router;
