be9c4c67b994996e0b7a296d7706ca7e428f6da0
AFelizzola <68514229+AFelizzola@users.noreply.github.com>
2021-11-10 22:16:32 -0300

    save user rol

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 async function auth(req, res, next){
     try {
         const token = req.header('Token');
-        jwt.verify(token, process.env.CLAVE_SECRETA);
+        const tokendecode = jwt.verify(token, process.env.CLAVE_SECRETA);
+        req.params.userid = tokendecode._id;
+        req.params.userrol = tokendecode.rol;
         next();
     } catch (error) {
         res.status(401).send({error: error.message});
